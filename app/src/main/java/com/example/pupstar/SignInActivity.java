0beb1098c1f92ee733719b132e87dc7d16cd27cd
MaleeshaSulakshana <mpmsulakshana98@gmail.com>
2022-08-10 01:37:25 +0530

    Added: implement sign in/up codes

--- app/src/main/java/com/example/pupstar/SignInActivity.java
+++ app/src/main/java/com/example/pupstar/SignInActivity.java
@@ -2,13 +2,41 @@ package com.example.pupstar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private LinearLayout txtSignUp;
+    private Button btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        txtSignUp = (LinearLayout) this.findViewById(R.id.txtSignUp);
+        btnSignIn = (Button) this.findViewById(R.id.btnSignIn);
+
+        txtSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignInActivity.this, DashboardActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/pupstar/SignUpActivity.java
+++ app/src/main/java/com/example/pupstar/SignUpActivity.java
@@ -2,13 +2,41 @@ package com.example.pupstar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private LinearLayout txtSignIn;
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        txtSignIn = (LinearLayout) this.findViewById(R.id.txtSignIn);
+        btnSignUp = (Button) this.findViewById(R.id.btnSignUp);
+
+        txtSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this, DashboardActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
