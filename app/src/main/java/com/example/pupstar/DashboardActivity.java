d11b0bf0793b9a2c6f6c60b52601f01c08acb9e1
MaleeshaSulakshana <mpmsulakshana98@gmail.com>
2022-08-31 21:52:37 +0530

    Added fragments with bottom navigator

--- app/src/main/java/com/example/pupstar/DashboardActivity.java
+++ app/src/main/java/com/example/pupstar/DashboardActivity.java
@@ -1,14 +1,89 @@
 package com.example.pupstar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.pupstar.Fragments.AppointmentsFragment;
+import com.example.pupstar.Fragments.ExploreFragment;
+import com.example.pupstar.Fragments.ProfileFragment;
+import com.example.pupstar.Fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        bottomNavigationView = (BottomNavigationView) this.findViewById(R.id.navigatorView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
+//        bottomNavigationView.setSelectedItemId(R.id.nav_search);
+//
+//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//
+//                switch (item.getItemId()) {
+//                    case R.id.nav_search:
+//                        startActivity(new Intent(getApplicationContext()
+//                            , SignInActivity.class));
+//                        overridePendingTransition(0, 0);
+//                        return true;
+//                }
+//
+//                return false;
+//            }
+//        });
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_search:
+                            selectedFragment = new SearchFragment();
+                            break;
+
+                        case R.id.nav_appointment:
+                            selectedFragment = new AppointmentsFragment();
+                            break;
+
+//                        case R.id.nav_explore:
+//                            selectedFragment = new ExploreFragment();
+//                            break;
+
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+
+                        case R.id.nav_sign_out:
+                            startActivity(new Intent(getApplicationContext()
+                                , SignInActivity.class));
+                            overridePendingTransition(0, 0);
+                            return true;
+
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+
 }
---
+++ app/src/main/java/com/example/pupstar/Fragments/AppointmentsFragment.java
@@ -0,0 +1,22 @@
+package com.example.pupstar.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pupstar.R;
+
+
+public class AppointmentsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_appointments, container, false);
+    }
+}
