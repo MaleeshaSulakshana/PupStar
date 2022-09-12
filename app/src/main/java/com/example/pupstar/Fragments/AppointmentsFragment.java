1ce7fa11bb83d551e1e4a728353c2993692768f0
MaleeshaSulakshana <mpmsulakshana98@gmail.com>
2022-09-13 01:01:46 +0530

    Added: Few implementations

--- app/src/main/java/com/example/pupstar/Fragments/AppointmentsFragment.java
+++ app/src/main/java/com/example/pupstar/Fragments/AppointmentsFragment.java
@@ -1,23 +1,37 @@
 package com.example.pupstar.Fragments;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.pupstar.R;
 import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 
 public class AppointmentsFragment extends Fragment {
 
     private ChipGroup chipGroup;
-    private LinearLayout layoutUpcoming, layoutPrevious;
+    private LinearLayout layoutUpcoming;
+
+    private ListView listView;
+    private ArrayList<AppointmentItem> arrayList = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,7 +42,9 @@ public class AppointmentsFragment extends Fragment {
         chipGroup = (ChipGroup) view.findViewById(R.id.chipGroup);
 
         layoutUpcoming = (LinearLayout) view.findViewById(R.id.layoutUpcoming);
-        layoutPrevious = (LinearLayout) view.findViewById(R.id.layoutPrevious);
+        listView = (ListView) view.findViewById(R.id.listView);
+
+        showUpcoming();
 
         chipGroup.setOnCheckedChangeListener(new ChipGroup.OnCheckedChangeListener() {
             @Override
@@ -54,13 +70,81 @@ public class AppointmentsFragment extends Fragment {
 
     private void showUpcoming()
     {
-        layoutUpcoming.setVisibility(View.VISIBLE);
-        layoutPrevious.setVisibility(View.GONE);
+
+        arrayList.clear();
+        listView.setAdapter(null);
+
+        AppointmentAdapter appointmentAdapter = new AppointmentAdapter(getContext(), R.layout.row_appointment_item, arrayList);
+        listView.setAdapter(appointmentAdapter);
+
+        arrayList.add(new AppointmentItem("1", "2022-10-02", "10.00"));
+        arrayList.add(new AppointmentItem("2", "2022-12-12", "11.00"));
+        appointmentAdapter.notifyDataSetChanged();
+
     }
 
     private void showPrevious()
     {
-        layoutPrevious.setVisibility(View.VISIBLE);
-        layoutUpcoming.setVisibility(View.GONE);
+        arrayList.clear();
+        listView.setAdapter(null);
+
+        AppointmentAdapter appointmentAdapter = new AppointmentAdapter(getContext(), R.layout.row_appointment_item, arrayList);
+        listView.setAdapter(appointmentAdapter);
+
+        arrayList.add(new AppointmentItem("1", "2022-5-02", "10.00"));
+        arrayList.add(new AppointmentItem("2", "2022-6-12", "11.00"));
+        appointmentAdapter.notifyDataSetChanged();
     }
+}
+
+class AppointmentItem {
+
+    String id, date, time;
+
+    public AppointmentItem(String id, String date, String time) {
+        this.id = id;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
+
+class AppointmentAdapter extends ArrayAdapter<AppointmentItem> {
+
+    private Context mContext;
+    private int mResource;
+
+    public AppointmentAdapter(@NonNull Context context, int resource, @NonNull ArrayList<AppointmentItem> objects) {
+        super(context, resource, objects);
+
+        this.mContext = context;
+        this.mResource = resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+        convertView = layoutInflater.inflate(mResource, parent, false);
+
+        TextView txtDate = (TextView) convertView.findViewById(R.id.txtDate);
+        TextView txtTime = (TextView) convertView.findViewById(R.id.txtTime);
+
+        txtDate.setText(getItem(position).getDate());
+        txtTime.setText(getItem(position).getTime());
+
+        return convertView;
+    }
+
 }
---
+++ app/src/main/java/com/example/pupstar/VeterinariansActivity.java
@@ -0,0 +1,14 @@
+package com.example.pupstar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VeterinariansActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_veterinarians);
+    }
+}
